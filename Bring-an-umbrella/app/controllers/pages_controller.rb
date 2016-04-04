a28d57cd7c042b28a64d0756e979392cb6c8d409
Matt Mohr <mmohr@covermymeds.com>
2016-04-03 21:39:16 -0400

    Added wunderground API usage wrapped in a controller action

--- Bring-an-umbrella/app/controllers/pages_controller.rb
+++ Bring-an-umbrella/app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
+require 'open-uri'
+
 class PagesController < ApplicationController
 
   def home
   end
 
-  
+  def api
+    zip = params[:zip]
+    uri = "http://api.wunderground.com/api/" + Rails.application.secrets.wunder_key_d + "/hourly/q/" + zip + ".json"
+    j = JSON.parse(open(uri).read)
+    render json: j
+  end
 end
